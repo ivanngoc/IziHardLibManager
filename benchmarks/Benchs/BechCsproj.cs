64946b0257c489efcab66be22909d68a0fa551c5
Tran Ngoc Anh <adycon94@gmail.com>
2024-12-08 19:27:28 +0300

    + benchs

---
+++ benchmarks/Benchs/BechCsproj.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+using BenchmarkDotNet.Attributes;
+using IziHardGames.IziProjectsManager.Common.Dtos;
+using Microsoft.AspNetCore.Mvc.Testing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Benchs
+{
+    [MemoryDiagnoser]
+    public class BechCsproj
+    {
+        private HttpClient apiClient;
+        private CsprojDto[]? dtos;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var factory = new WebApplicationFactory<IziLibraryApiGate.Program>();
+            //.WithWebHostBuilder(configuration =>
+            //{
+
+            //});
+
+            this.apiClient = factory.CreateClient();
+
+            var response =  this.apiClient.GetAsync("api/Tables/Csprojs").GetAwaiter().GetResult();
+            this.dtos = response.Content.ReadFromJsonAsync<CsprojDto[]>().GetAwaiter().GetResult();
+        }
+
+        [Benchmark]
+        public void Single()
+        {
+            var ss = 1.ToString();
+            foreach (var dto in dtos)
+            {
+                string s = dto.ToString();
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ benchmarks/Benchs/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchs
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+#if DEBUG
+            var d = new BechCsproj();
+            d.Setup();
+            d.Single();
+#else
+            BenchmarkRunner.Run<BechCsproj>();
+#endif
+        }
+    }
+}
